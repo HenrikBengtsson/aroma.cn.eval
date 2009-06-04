077dedfd1068f440ce6841ef59f2a16312caf13e
Henrik Bengtsson <hb@aroma-project.org>
2009-06-04 23:21:24 +0000

    SVN move
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/aroma-dots/pkg/aroma.cn.eval@1518 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ R/006.fixVarArgs.R
@@ -0,0 +1,10 @@
+# Added '...' to some base functions. These will later be
+# turned into default functions by setMethodS3().
+smooth <- appendVarArgs(smooth);
+
+
+############################################################################
+# HISTORY:
+# 2009-02-06
+# o Created to please R CMD check.
+############################################################################
