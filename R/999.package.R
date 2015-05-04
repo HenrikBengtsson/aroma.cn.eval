aee3e26fff6e7f94ccd23ee1b32c8893c663a5e5
hb <hb@aroma-project.org>
2015-05-04 15:30:40 -0700

    ROBUSTNESS: Forgot to declare one S3 method in NAMESPACE and explicitly import R.utils::GenericSummary.

--- R/999.package.R
+++ R/999.package.R
@@ -2,33 +2,27 @@
 # @RdocPackage aroma.cn.eval
 #
 # \description{
-#   @eval "getDescription(aroma.cn.eval)" 
+#   @eval "getDescription(aroma.cn.eval)"
 #
 #   This package should be considered to be in an alpha phase.
 #   You should expect the API to be changing over time.
 #   Consider this package a private package.
 # }
 #
-# \section{Installation and updates}{
-#
-#   To install this package, see instructions at 
-#   \url{@eval "getUrl(aroma.cn.eval)"}.
-# } 
-#
 # \author{
 #   Henrik Bengtsson.
 # }
 #
 # \section{License}{
-#   The releases of this package is licensed under 
+#   The releases of this package is licensed under
 #   LGPL version 2.1 or newer.
 #
-#   The development code of the packages is under a private licence 
+#   The development code of the packages is under a private licence
 #   (where applicable) and patches sent to the author fall under the
 #   latter license, but will be, if incorporated, released under the
 #   "release" license above.
 # }
-# 
+#
 # \references{
 #   TBA.
 # }
